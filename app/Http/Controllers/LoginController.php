339aed0ddc7dcfdb7c7ee7a4f69a3290722f7df8
ukyo <ukyo99@gmail.com>
2021-01-19 11:35:53 +0800

    fix view and jquery

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,85 +2,70 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Auth;
 use App\User;
-
+use Auth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller {
-	
+
 	//首頁跳轉
-	public function homepage() { 
-		return view('homepage');		
+	public function homepage() {
+		return view('homepage');
 	}
-	
+
 	//login頁面跳轉
-	public function loginpage(){ 
+	public function loginpage() {
 		return view('login');
 	}
 
 	//Userlist跳轉&讀取DB
-	public function userlist(Request $request){
-		$data=User::paginate(10);
+	public function userlist(Request $request) {
+		$data = User::paginate(10);
 		// $data->setPath('userlist/2');
 		// echo "<pre>";
 		// print_r($data->all());
-		return view('userlist',['data'=>$data]);
+		return view('userlist', ['data' => $data]);
 	}
-	
+
 	//使用者登出
-	public function logout(Request $request){
-		if(Auth::check()){
+	public function logout(Request $request) {
+		if (Auth::check()) {
 			Auth::logout();
 			return redirect()->intended('/');
-		}else{
+		} else {
 			return back();
 		}
-			
+
 	}
 
-
-
-
-
-
-
-	
-
-
-
-
 	// public function login(){
-    //     if(Auth::check()){
-    //         echo 'Auth status: yes'; 
-    //         return view('login2');
+	//     if(Auth::check()){
+	//         echo 'Auth status: yes';
+	//         return view('login2');
 	// 	}else{
-	// 		echo 'Auth status: no'; 
+	// 		echo 'Auth status: no';
 	// 		return view('login');
 	// 	}}
-	
-	
 
-	public function ajaxcrud(Request $request){
+	public function ajaxcrud(Request $request) {
 		// $insert=User::Create();
-			// $name->request->account,
+		// $name->request->account,
 		echo '<pre>';
 		print_r($request);
 		// ]);
 
-
-        // DB::table('users')->insert([
-        //     'account' =>'aaaaa',
-        //     'name' => '99999',
-        //     'password' => Hash::make('99999'),
-        //     'gender' =>'male',
-        //     'remember_check' =>'ok',
-        //     'remember_token' =>'NULL',
-        //     'cellphone' => '0987987987',
-        //     'created_at' => date('Y-m-d H:i:s'),
-        //     'updated_at' => date('Y-m-d H:i:s',$newtime),
-        // ]);
+		// DB::table('users')->insert([
+		//     'account' =>'aaaaa',
+		//     'name' => '99999',
+		//     'password' => Hash::make('99999'),
+		//     'gender' =>'male',
+		//     'remember_check' =>'ok',
+		//     'remember_token' =>'NULL',
+		//     'cellphone' => '0987987987',
+		//     'created_at' => date('Y-m-d H:i:s'),
+		//     'updated_at' => date('Y-m-d H:i:s',$newtime),
+		// ]);
 
 	}
-	
+
 }
